f474ecabdaa7c1d6b3d2e90f35117b08e8c9b158
vargasjuanibarra <vargasjuanibarra@gmail.com>
2024-09-12 23:58:04 +0800

    create navigation component and added to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
+import Nav from "@components/Nav";
 import "@styles/globals.css";
+import { ReactNode } from "react";
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover @Share AI Prompts",
 };
 
-const RootLayout = () => {
+const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
       <body>
         <div className="main">
           <div className="gradient"></div>
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
